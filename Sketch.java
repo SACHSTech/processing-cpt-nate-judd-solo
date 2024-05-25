0985079b14de78bcfa70acbfc45e138d7029d8cf
NateJudd <nathaniel.judd25@ycdsbk12.ca>
2024-05-25 12:34:43 -0400

    Allows for basic character movement on the screen. Character can not interact with anything but can run and jump;

--- Sketch.java
+++ Sketch.java
@@ -2,33 +2,57 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
-  // variables and images used for the background
+  // Variables and images used for the background
   PImage imgMainBG;
   int intScreenW = 800;
-  int intScreenH = 600;
-  int intSpeedBG = 2;
-	float fltXPosBG = 0;
-	
+  int intScreenH = 700;
+  int intSpeedBG = 3;
+  float fltXPosBG = 0;
+
+  // Variables and images used for the main character
+  PImage imgMC;
+  int intWidthMC = 60;
+  int intHeightMC = 70;
+  int intSpeed;
+  int intWalk = 3;
+  int intSprint = 5;
+  int intJumpSpeed = 5;
+  int intJumpDuration = 30;
+  float fltXPos = 100;
+  float fltYPos = 400;
+  float fltJumpHeight = 125;
+  float fltFallSpeed = 5;
+  float fltPreJumpYPos = fltYPos;
+  boolean blnJump = false;
+  boolean blnLeft = false;
+  boolean blnRight = false;
+  boolean blnPeakReached = false;
+  boolean blnSprint = false;
+
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Initializes background size
    */
   public void settings() {
-    // Background Size
+    // Background size
     size(intScreenW, intScreenH);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+  /**
+   * Sets up the initial environment
    */
   public void setup() {
     // Loads the main background image
     imgMainBG = loadImage("MainBG.jpeg");
     imgMainBG.resize(intScreenW, intScreenH);
+
+    // Loads the main character image
+    imgMC = loadImage("MainCharacter.png");
+    imgMC.resize(intWidthMC, intHeightMC);
+
   }
 
   /**
-   * Called repeatedly, anything drawn to the screen goes here
+   * Top level method to execute the program
    */
   public void draw() {
     // Clears the background
@@ -38,12 +62,83 @@ public class Sketch extends PApplet {
     image(imgMainBG, fltXPosBG, 0);
     image(imgMainBG, fltXPosBG + intScreenW, 0);
 
+    // Changes background speed when sprinting
+    if (blnSprint) {
+      intSpeedBG = intSprint;
+    } else {
+      intSpeedBG = intWalk;
+    }
+
     // Updates backgorund to achieve scrolling effect
-    fltXPosBG -= intSpeedBG;
+    if (blnRight && fltXPos > width / 2 - 75) { // substracting 75 gives the player more vision in front of them
+      fltXPosBG -= intSpeedBG;
+    } else if (blnRight) {
+      fltXPos += intSpeed;
+    }
     if (fltXPosBG <= -width) {
       fltXPosBG = 0;
     }
+
+    // Main character's horizontal movement
+    if (blnLeft) {
+      fltXPos -= intSpeed;
+    }
+
+    // Main character's sprinting
+    if (blnSprint) {
+      intSpeed = intSprint;
+    } else {
+      intSpeed = intWalk;
+    }
+
+    // Checks if the main character has jumped
+    if (blnJump) {
+      // Alters main characters y position until they reach the peak of their jump
+      if (fltYPos > fltPreJumpYPos - fltJumpHeight && blnPeakReached == false) {
+        fltYPos -= intJumpSpeed;
+      } else if (fltYPos < fltPreJumpYPos) { // Going down after jumping
+        fltFallSpeed += 0.1;
+        fltYPos += fltFallSpeed;
+        blnPeakReached = true;
+      } else if (fltYPos >= fltPreJumpYPos) { // Resets so  can jump again
+        fltYPos = fltPreJumpYPos;
+        blnPeakReached = false;
+        blnJump = false;
+        fltFallSpeed = 4;
+      }
+    }
+
+    // Prints the main character to the screen
+    image(imgMC, fltXPos, fltYPos);
   }
-  
-  // define other methods down here.
+
+  /**
+   * Checks if keys have been pressed
+   */
+  public void keyPressed() {
+    // main character movement
+    if (keyCode == UP) {
+      blnJump = true;
+    } else if (keyCode == LEFT) {
+      blnLeft = true;
+    } else if (keyCode == RIGHT) {
+      blnRight = true;
+    } else if (key == 'z') {
+      blnSprint = true;
+    }
+  }
+
+  /**
+   * Checks if keys have been pressed
+   */
+  public void keyReleased() {
+    // Main character movement
+    if (keyCode == LEFT) {
+      blnLeft = false;
+    } else if (keyCode == RIGHT) {
+      blnRight = false;
+    } else if (key == 'z') {
+      blnSprint = false;
+    }
+  }
 }
